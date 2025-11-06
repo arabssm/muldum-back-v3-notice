5583274f5e1734bdd77980c465dc4684b51d6046
hxeonxins <hyunjin.dev143@gmail.com>
2025-11-06 09:46:15 +0900

    fix: MuldumApplication으로 수정

--- src/main/java/co/kr/muldum/MuldumApplication.java
+++ src/main/java/co/kr/muldum/MuldumApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MuldumNoticeApplication {
+public class MuldumApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MuldumNoticeApplication.class, args);
+        SpringApplication.run(MuldumApplication.class, args);
     }
 
 }
