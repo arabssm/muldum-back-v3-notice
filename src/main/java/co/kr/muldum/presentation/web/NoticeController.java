3cc1baa614a58725fead8faf568ba1b2d46138a7
hxeonxins <hyunjin.dev143@gmail.com>
2025-11-12 14:05:33 +0900

    refactor: change notice endpoint path to /ara/notice

--- src/main/java/co/kr/muldum/presentation/web/NoticeController.java
+++ src/main/java/co/kr/muldum/presentation/web/NoticeController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/notice")
+@RequestMapping("/ara/notice")
 @RequiredArgsConstructor
 public class NoticeController {
 
